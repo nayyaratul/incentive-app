054f1387abe5446d741948a68b404a1047ba6c20
Atul Nayyar <atul.nayyar@betterplace.co.in>
2026-04-13 12:16:25 +0530

    chore: babel config (preset-env + preset-react automatic)

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { browsers: ['>0.2%', 'not dead', 'not op_mini all'] } }],
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ]
+};
